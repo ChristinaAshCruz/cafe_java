fc537b01f9ec10ddc60caeb6ac3cc9a15a179269
ChristinaAshCruz <christina.ash.cruz@gmail.com>
2023-02-10 00:15:08 -0800

    implemented assignment steps - orders

--- CafeJava.java
+++ CafeJava.java
@@ -9,16 +9,62 @@ public class CafeJava {
         
         // Menu variables (add yours below)
         double mochaPrice = 3.5;
+        double lattePrice = 1.75;
+        double cappucinoPrice = 5.25;
+        double coffeePrice = 3.00;
     
         // Customer name variables (add yours below)
         String customer1 = "Cindhuri";
+        String customer2 = "Sam Cooke";
+        String customer3 = "Jimmy Fallon";
+        String customer4 = "Noah Wyle";
     
         // Order completions (add yours below)
         boolean isReadyOrder1 = false;
+        boolean isReadyOrder2 = true;
+        boolean isReadyOrder3 = false;
+        boolean isReadyOrder4 = true;
     
         // APP INTERACTION SIMULATION (Add your code for the challenges below)
-        // Example:
-        System.out.println(generalGreeting + customer1); // Displays "Welcome to Cafe Java, Cindhuri"
-    	// ** Your customer interaction print statements will go here ** //
+
+        // customer1 - app interactions:
+        System.out.println(generalGreeting + customer1 + "!");
+        
+        if (isReadyOrder1){
+            System.out.println(customer1 + readyMessage);
+            System.out.println(displayTotalMessage + coffeePrice);
+        } else {
+            System.out.println(customer1 + pendingMessage);
+        }
+
+        System.out.println("---------------------------------------");
+
+        // Noah - interactions
+        System.out.println(generalGreeting + customer4 + "!");
+        
+        System.out.println(customer4 + pendingMessage);
+        
+        if (isReadyOrder2){
+            System.out.println(customer4 + readyMessage);
+        } else {
+            System.out.println(customer4 + pendingMessage);
+        }
+
+        System.out.println("---------------------------------------");
+
+        // Sam - interactions
+        System.out.println(generalGreeting + customer2 + "!");
+        System.out.println(displayTotalMessage + String.format("%.2f",(2 * cappucinoPrice)));
+
+        if (isReadyOrder3){
+            System.out.println(customer3 + readyMessage);
+        } else {
+            System.out.println(customer3 + pendingMessage);
+        }
+
+        System.out.println("---------------------------------------");
+
+        // Jimmy interactions
+        System.out.println("Sorry, Jimmy, you still owe us $" + String.format("%.2f",(cappucinoPrice - coffeePrice)));
     }
 }
